862c86ed92bb844a13119de546cc42c378aa9409
Kleber Landivar <kleberlandivar93@gmail.com>
2023-11-23 08:40:31 -0500

    Página de MyOrders: lista de órdenes

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react'
+import { Link } from 'react-router-dom'
 import { XMarkIcon } from '@heroicons/react/24/solid'
 import { ShoppingCartContext } from '../../Context'
 import OrderCard from '../../Components/OrderCard'
@@ -39,14 +40,14 @@ const CheckoutSideMenu = () => {
       <div className='px-6 overflow-y-scroll flex-1'>
         {
           context.cartProducts.map(product => (
-            <OrderCard 
-              key={product.id}
-              id={product.id}
-              title={product.title} 
-              images={product.images}
-              price={product.price}
-              handleDelete={handleDelete}
-            />
+              <OrderCard 
+                key={product.id}
+                id={product.id}
+                title={product.title} 
+                images={product.images}
+                price={product.price}
+                handleDelete={handleDelete}
+              />
           ))
         }
       </div>
@@ -55,7 +56,9 @@ const CheckoutSideMenu = () => {
           <span className='font-light'>Total:</span>
           <span className='font-medium text-2xl'>${totalPrice(context.cartProducts)}</span>
         </p>
-        <button className='bg-black py-3 text-white w-full rounded-lg' onClick={() => handleCheckout()}>Checkout</button>
+        <Link to='my-orders/last'>
+          <button className='bg-black py-3 text-white w-full rounded-lg' onClick={() => handleCheckout()}>Checkout</button>
+        </Link>
       </div>
     </aside>
   )
