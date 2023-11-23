76f044efaa01fd140f27af229d2966036fb25f3a
Kleber Landivar <kleberlandivar93@gmail.com>
2023-11-22 22:23:39 -0500

    Eliminar productos del carrito

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -6,6 +6,11 @@ import './styles.css'
 
 const CheckoutSideMenu = () => {
   const context = useContext(ShoppingCartContext);
+
+  const handleDelete = (id) => {
+    const filteredProducts = context.cartProducts.filter(product => product.id != id);
+    context.setCartProducts(filteredProducts)
+  }
   
   return (
     <aside
@@ -23,9 +28,11 @@ const CheckoutSideMenu = () => {
           context.cartProducts.map(product => (
             <OrderCard 
               key={product.id}
+              id={product.id}
               title={product.title} 
               images={product.images}
               price={product.price}
+              handleDelete={handleDelete}
             />
           ))
         }
