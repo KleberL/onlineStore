d33edbd4848a3f4c75d15b58e4b062ca8a188a66
Kleber Landivar <kleberlandivar93@gmail.com>
2023-11-24 15:24:21 -0500

    Filtrando títulos con JavaScript

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -27,16 +27,24 @@ export const ShoppingCartProvider = ({ children }) => {
 
     // Get Products
     const [items, setItems] = useState(null);
+    const [filteredItems, setFilteredItems] = useState(null);
 
     // Get Products by Title
     const [searchByTitle, setSearchByTitle] = useState(null);
-    console.log(searchByTitle)
 
     useEffect(() => {
         fetch('https://fakestoreapi.com/products')
           .then(response => response.json())
           .then(data => setItems(data))
       }, [])
+
+    const filteredItemsByTitle = (items, searchByTitle) =>{
+        return items?.filter(item => item.title.toLowerCase().includes(searchByTitle.toLowerCase()))
+    }
+
+    useEffect(() => {
+        if(searchByTitle) setFilteredItems(filteredItemsByTitle(items, searchByTitle))
+      }, [items, searchByTitle])
 
     return(
         <ShoppingCartContext.Provider value={{
@@ -57,7 +65,9 @@ export const ShoppingCartProvider = ({ children }) => {
             items,
             setItems,
             searchByTitle,
-            setSearchByTitle
+            setSearchByTitle,
+            filteredItems,
+            setFilteredItems,
         }}>
             {children}
         </ShoppingCartContext.Provider>
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,32 +1,42 @@
-import { useContext } from "react"
-import Layout from "../../Components/Layout"
-import Card from "../../Components/Card"
+import { useContext } from "react";
+import Layout from "../../Components/Layout";
+import Card from "../../Components/Card";
 import ProductDetail from "../../Components/ProductDetail";
 import { ShoppingCartContext } from "../../Context";
 
 function Home() {
-  const { items, setSearchByTitle } = useContext(ShoppingCartContext)
+  const { items, searchByTitle, setSearchByTitle, filteredItems } =
+    useContext(ShoppingCartContext);
+
+  const renderView = () => {
+    if (searchByTitle?.length > 0) {
+      if (filteredItems?.length > 0){
+        return filteredItems?.map((item) => <Card key={item.id} data={item} />);
+      } else {
+        return <div>We don't have anything</div>
+      }
+    } else {
+      return items?.map((item) => <Card key={item.id} data={item} />);
+    }
+  };
+
   return (
     <Layout>
-      <div className='flex items-center justify-center relative w-80 mb-4'>
-        <h1 className='font-medium text-xl'>Exclusive Products</h1>
+      <div className="flex items-center justify-center relative w-80 mb-4">
+        <h1 className="font-medium text-xl">Exclusive Products</h1>
       </div>
-      <input 
+      <input
         type="text"
-        placeholder='Search a product'
-        className='rounded-lg border border-black w-80 p-4 mb-4 focus:outline-none'
-        onChange={(e) => setSearchByTitle(e.target.value)}  
+        placeholder="Search a product"
+        className="rounded-lg border border-black w-80 p-4 mb-4 focus:outline-none"
+        onChange={(e) => setSearchByTitle(e.target.value)}
       />
       <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
-        {
-          items?.map(item => (
-            <Card key={item.id} data={item}/>
-          ))
-        }
+        {renderView()}
       </div>
-      <ProductDetail/>
+      <ProductDetail />
     </Layout>
-  )
+  );
 }
 
-export default Home
+export default Home;
